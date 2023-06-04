2d9c2886938c62d3423bc5eeda90d5ecb040334b
Bentz <benakiva1991@gmail.com>
2023-06-04 15:22:46 +0300

    added service with http call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreatureListComponent } from './creature/creature-list/creature-list.component';
 import { CreatureCardComponent } from './creature/creature-card/creature-card.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CreaturesService } from './services/creatures.service';
 
 @NgModule({
-  declarations: [AppComponent, CreatureCardComponent, CreatureListComponent, NavBarComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    CreatureCardComponent,
+    CreatureListComponent,
+    NavBarComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [CreaturesService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/creature/creature-list/creature-list.component.ts
+++ src/app/creature/creature-list/creature-list.component.ts
@@ -1,72 +1,24 @@
-import { Component } from '@angular/core';
-import { Creature, CreatureFamily } from '../creature.interface';
+import { Component, OnInit } from '@angular/core';
+import { CreaturesService } from 'src/app/services/creatures.service';
 
 @Component({
   selector: 'app-creature-list',
   templateUrl: './creature-list.component.html',
   styleUrls: ['./creature-list.component.scss'],
 })
-export class CreatureListComponent {
-  creatures: Creature[] = [
-    {
-      id: 1,
-      name: 'Fluffy',
-      family: CreatureFamily.fire,
-      strength: 200,
-    },
-    {
-      id: 2,
-      name: 'Bubbles',
-      family: CreatureFamily.water,
-      strength: 500,
-    },
-    {
-      id: 3,
-      name: 'Rocky',
-      family: CreatureFamily.earth,
-      strength: 800,
-    },
-    {
-      id: 4,
-      name: 'Zephyr',
-      family: CreatureFamily.wind,
-      strength: 300,
-    },
-    {
-      id: 5,
-      name: 'Sparkle',
-      family: CreatureFamily.light,
-      strength: 1000,
-    },
-    {
-      id: 6,
-      name: 'Midnight',
-      family: CreatureFamily.dark,
-      strength: 1200,
-    },
-    {
-      id: 7,
-      name: 'Ember',
-      family: CreatureFamily.fire,
-      strength: 150,
-    },
-    {
-      id: 8,
-      name: 'Splash',
-      family: CreatureFamily.water,
-      strength: 700,
-    },
-    {
-      id: 9,
-      name: 'Pebble',
-      family: CreatureFamily.earth,
-      strength: 400,
-    },
-    {
-      id: 10,
-      name: 'Zephyrus',
-      family: CreatureFamily.wind,
-      strength: 900,
-    },
-  ];
+export class CreatureListComponent implements OnInit {
+  creatures: any;
+
+  constructor(private creaturesService: CreaturesService) {}
+
+  ngOnInit(): void {
+    this.creaturesService.getCreatures().subscribe(
+      (data) => {
+        this.creatures = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
